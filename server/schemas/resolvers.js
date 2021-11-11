a0a1ab917ec710037a3865d091b70c9a95ce7f87
Navleen <navleenkaurr@gmail.com>
2021-11-11 12:46:29 -0500

    Set up query and mutation functionalities for User in resolvers

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,50 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User, Book } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+    // Access current user's profile
+    me: async (parent, args, context) => {
+        if (context.user) {
+          // excludes password form User object
+          const userData = await User.findOne({ _id: context.user._id }).select('-__v -password');
+          return userData;
+        }
+        throw new AuthenticationError('Not logged in!');
+      }
+    },
+    Mutation: {
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+      
+            return { token, user };
+          },
+          login: async (parent, { email, username, password }) => {
+            // Look up the user by the provided email address. Since the `email` field is unique, we know that only one person will exist with that email
+            const user = await User.findOne({ $or: [{ email: email }, { username: username }] });
+      
+            if (!user) {
+              throw new AuthenticationError('Incorrect credentials');
+            }
+      
+            const checkPassword = await user.checkPassword(password);
+      
+            if (!checkPassword) {
+              throw new AuthenticationError('Incorrect credentials');
+            }
+      
+            const token = signToken(user);
+            return { token, user };
+          },
+          saveBook: async () => {
+
+          },
+          deleteBook: async () => {
+              
+          }
+    }
+}
+
+module.exports = resolvers;
