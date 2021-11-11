c4ed61dc2e74be7d16d16c768fa3c3077dd92df8
Navleen <navleenkaurr@gmail.com>
2021-11-11 13:10:04 -0500

    Set up savedBooks mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,49 +1,57 @@
 const { AuthenticationError } = require('apollo-server-express');
-const { User, Book } = require('../models');
+const { User } = require('../models');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
-    // Access current user's profile
-    me: async (parent, args, context) => {
-        if (context.user) {
-          // excludes password form User object
-          const userData = await User.findOne({ _id: context.user._id }).select('-__v -password');
-          return userData;
+        // Access current user's profile
+        me: async (parent, args, context) => {
+            if (context.user) {
+                // excludes password form User object
+                const userData = await User.findOne({ _id: context.user._id }).select('-__v -password');
+                return userData;
+            }
+            throw new AuthenticationError('Not logged in!');
         }
-        throw new AuthenticationError('Not logged in!');
-      }
     },
     Mutation: {
         addUser: async (parent, args) => {
             const user = await User.create(args);
             const token = signToken(user);
-      
+
             return { token, user };
-          },
-          login: async (parent, { email, username, password }) => {
+        },
+        login: async (parent, { email, username, password }) => {
             // Look up the user by the provided email address. Since the `email` field is unique, we know that only one person will exist with that email
             const user = await User.findOne({ $or: [{ email: email }, { username: username }] });
-      
+
             if (!user) {
-              throw new AuthenticationError('Incorrect credentials');
+                throw new AuthenticationError('Incorrect credentials');
             }
-      
+
             const checkPassword = await user.checkPassword(password);
-      
+
             if (!checkPassword) {
-              throw new AuthenticationError('Incorrect credentials');
+                throw new AuthenticationError('Incorrect credentials');
             }
-      
+
             const token = signToken(user);
             return { token, user };
-          },
-          saveBook: async () => {
-
-          },
-          deleteBook: async () => {
-              
-          }
+        },
+        saveBook: async (parent, { bookData }, context) => {
+            if (context.user) {
+                const user = await User.updateOne(
+                    { _id: context.user._id },
+                    { $push: { savedBooks: { bookData } } },
+                    { new: true }
+                );
+                return user;
+            }
+            throw new AuthenticationError('User not logged in');
+        },
+        deleteBook: async (parent, { bookId }) => {
+            
+        }
     }
 }
 
