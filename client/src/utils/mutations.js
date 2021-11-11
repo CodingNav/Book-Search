9b60226007d69cd63726c0fb8a84d90def3970af
Navleen <navleenkaurr@gmail.com>
2021-11-11 13:27:09 -0500

    Set up save_book mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -23,3 +23,19 @@ export const ADD_USER = gql`
     }
   }
 `;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($bookId: String, $authors: [String], $description: String, $title: String, $image: String, $link: String) {
+      saveBook(bookId: $bookId, authors: $authors, description: $description, title: $title, image: $image, link: $link) {
+          user {
+            _id
+            username
+            savedBooks
+          }
+      }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+
+`;
